8527928d949c5c3a99f7f4b8113c8111e0caf95b
Reihannnnniy <144640316+Reihannnnniy@users.noreply.github.com>
2023-09-11 09:18:06 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+int a,b;
+cout<<"PEMROGRAMAN OPERASI ARITMATIKA \n";
+cout<<"masukan angka pertama: ";
+cin>>a;
+cout<<"masukan angka kedua: ";
+cin>>b;
+cout<<"Hasil penjumlahan: "<<a+b<< endl;
+cout<<"Hasil pengurangan: "<<a-b<< endl;
+cout<<"Hasil perkalian: "<<a*b<< endl;
+cout<<"Hasil pembagian: "<<a/b<< endl;
+cout<<"Hasil mudulus: "<<a%b<< endl;
+return 0;
+}
